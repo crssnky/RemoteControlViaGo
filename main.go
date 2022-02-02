b65c08e41033e12a75be4d43a911c830c18e7704
crssnky <vindicare28@yahoo.co.jp>
2022-02-03 01:01:56 +0900

    send in UDP from http/template

--- main.go
+++ main.go
@@ -1,29 +1,67 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
+	"strconv"
 	"text/template"
+
+	"github.com/spf13/viper"
 )
 
 var templates = make(map[string]*template.Template)
+var host string
+var conn net.Conn
+var err error
 
 const ColorGrading = "ColorGrading"
 
 func main() {
+	viper.SetConfigName("setting")
+	viper.SetConfigType("yaml")
+	viper.SetDefault("host.ip", "localhost")
+	viper.SetDefault("host.port", 8080)
+	viper.AddConfigPath(".")
+	if err := viper.ReadInConfig(); err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	host = viper.GetString("host.ip") + ":" + strconv.Itoa(viper.GetInt("host.port"))
+	fmt.Println("Hello, This is RemoteControlViaGo from " + host)
+
+	conn, err = net.Dial("udp", "172.29.49.211:8081")
+	if err != nil {
+		panic(fmt.Errorf("fatal error dial udp: %w", err))
+	}
+	defer conn.Close()
+
 	templates[ColorGrading] = loadTemplate(ColorGrading)
 	http.HandleFunc("/", handleIndex)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/pp", handleIsUseTemperatureType)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(viper.GetInt("host.port")), nil))
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
 	if err := templates[ColorGrading].Execute(w, struct {
 		Title string
+		Host  string
 	}{
+		Host:  host,
 		Title: ColorGrading,
 	}); err != nil {
 		log.Printf("failed to execute template: %v", err)
 	}
+}
+
+func handleIsUseTemperatureType(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	bytearry, _ := ioutil.ReadAll(r.Body)
+	conn.Write(bytearry)
+	fmt.Println(string(bytearry))
 }
 
 func loadTemplate(name string) *template.Template {
